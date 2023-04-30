074654fa1d3655de85dfb41f149cb188f08ec8af
znajman <79368320+znajman@users.noreply.github.com>
2023-04-30 17:52:09 +0200

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+    if (!Number.isInteger(startNumber) || !Number.isInteger(endNumber)) return 'ERROR';
+    if (startNumber < 0 || endNumber < 0) return 'ERROR';
+    n = Math.abs(endNumber - startNumber) + 1;
+    return (startNumber + endNumber) * n / 2;
 };
 
 // Do not edit below this line
