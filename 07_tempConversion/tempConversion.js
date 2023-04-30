0bebd143fc8355af97505b42cc82a39353093f5e
znajman <79368320+znajman@users.noreply.github.com>
2023-04-30 18:17:48 +0200

    Complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return +((temperature - 32) * 5 / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return +(temperature * 9 / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
