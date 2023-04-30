45d784641537f9b80c04c5aaa8cb0faba275202f
znajman <79368320+znajman@users.noreply.github.com>
2023-04-30 16:51:28 +0200

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+/*const removeFromArray = function() {
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arguments[0].findIndex(element => element === arguments[i]);
+        if (index >= 0) {
+            arguments[0].splice(index, 1);
+        }
+    }
+    
+    return arguments[0];
+};*/
 
+const removeFromArray = function(array, ...args) {
+    args.forEach((item) => {
+
+        let index = array.findIndex(element => element === item);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    });
+    
+    return arguments[0];
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
