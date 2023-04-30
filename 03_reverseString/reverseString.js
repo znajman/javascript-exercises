0a4d14ccf8f69320e33bcb549d25d2a5f8eea751
znajman <79368320+znajman@users.noreply.github.com>
2023-04-30 15:47:30 +0200

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let stringReversed = '';
+    for (let i = stringToReverse.length; i > 0; i--) {
+        stringReversed += stringToReverse.substring(i - 1, i);
+    }
+    return stringReversed;
 };
 
 // Do not edit below this line
