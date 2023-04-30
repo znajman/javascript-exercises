b0d03eb30482d13221206e66acd87e2278b65d84
znajman <79368320+znajman@users.noreply.github.com>
2023-04-30 15:20:48 +0200

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, timesToRepeat) {
+    let returnString = '';
+    if (timesToRepeat < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < timesToRepeat; i++) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
