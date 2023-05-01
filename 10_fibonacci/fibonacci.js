bb1507e995e1bcd1025b24eb6832ff7b62440c8e
znajman <79368320+znajman@users.noreply.github.com>
2023-05-01 19:07:24 +0200

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 1) return 'OOPS';
+    if(n < 3) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
