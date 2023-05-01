fce9a02f13477b06e32f8873b232bba43c03a0ea
znajman <79368320+znajman@users.noreply.github.com>
2023-05-01 19:02:07 +0200

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = (string.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,'')).toLowerCase();
+    let palindrom = '';
+    for (let i = 0; i < newString.length; i++) {
+        palindrom += newString[newString.length-i-1];
+    }
+    console.log(palindrom);
+    return newString === palindrom;
 };
 
 // Do not edit below this line
