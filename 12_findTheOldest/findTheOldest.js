51b1579378c9409e5ddba7f76751a22fce46d127
znajman <79368320+znajman@users.noreply.github.com>
2023-05-01 20:05:38 +0200

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const ordered = people.sort((personA,personB) => {
+        aAge = ((Object.hasOwn(personA,'yearOfDeath') ? (personA.yearOfDeath - personA.yearOfBirth) : ((new Date().getFullYear())) - personA.yearOfBirth)) 
+        bAge = ((Object.hasOwn(personB,'yearOfDeath') ? (personB.yearOfDeath - personB.yearOfBirth) : ((new Date().getFullYear())) - personB.yearOfBirth)) 
+        return aAge < bAge ? 1 : -1;
+    });
+    return ordered[0];
 
 };
 
